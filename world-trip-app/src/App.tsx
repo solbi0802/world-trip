ee1b321392539651abbc94fe94aaea9c3e8b6a83
SolbiShin <sbshin.dev@gmail.com>
2022-11-22 23:52:49 +0900

    마우스오버시 나라이름 표시하도록

--- world-trip-app/src/App.tsx
+++ world-trip-app/src/App.tsx
@@ -1,11 +1,17 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './App.css';
 import MapChart from './MapChart';
+import ReactTooltip from "react-tooltip";
 
 function App() {
+  const [content,setContent] = useState<string>("")
+  useEffect(() => {
+console.log('content',content)
+  },[content])
   return (
     <div className="App">
-        <MapChart></MapChart>
+        <MapChart setTooltipContent={setContent}></MapChart>
+        <ReactTooltip>{content}</ReactTooltip>
     </div>
   );
 }
--- world-trip-app/src/MapChart.tsx
+++ world-trip-app/src/MapChart.tsx
@@ -1,24 +1,48 @@
-import React from "react";
-import {ComposableMap, Geographies,Geography, ZoomableGroup, Marker } from 'react-simple-maps';
+import React,{memo} from "react";
+import {ComposableMap, Geographies,Geography, ZoomableGroup } from 'react-simple-maps';
 
-const geoUrl =
-  'https://raw.githubusercontent.com/deldersveld/topojson/master/world-countries.json';
-
-const MapChart = ()  => {
+const MapChart = ({setTooltipContent})  => {
   return (
-    <ComposableMap projection="geoMercator">
-         <ZoomableGroup center={[0, 0]} zoom={9}>
-      <Geographies geography={geoUrl}>
-        {({ geographies }) =>
-          geographies.map(geo => <Geography key={geo.rsmKey} geography={geo} />)
-        }
-      </Geographies>
-      <Marker coordinates={[0, 0]}>
+    <div data-tip="">
+    <ComposableMap>
+      <ZoomableGroup>
+         <Geographies geography="/features.json">
+            {({ geographies }) =>
+              geographies.map((geo) => (
+                <Geography
+                  key={geo.rsmKey}
+                  geography={geo}
+                  onMouseEnter={() => {
+                    setTooltipContent(geo.properties.kor ? `${geo.properties.kor}` : `${geo.properties.name}`);
+                  }}
+                  onMouseLeave={() => {
+                    setTooltipContent("");
+                  }}
+                  style={{
+                    default: {
+                      fill: "#b7b7bb",
+                      outline: "none"
+                    },
+                    hover: {
+                      fill: "#97a3ea",
+                      outline: "none"
+                    },
+                    pressed: {
+                      fill: "#5493b5",
+                      outline: "none"
+                    }
+                  }}
+                />
+              ))
+            }
+          </Geographies>
+      {/* <Marker coordinates={[0, 0]}>
             <circle r={3} fill="#d586d3" />
-          </Marker>
+          </Marker> */}
         </ZoomableGroup>
     </ComposableMap>
+    </div>
   );
 }
 
-export default MapChart;
+export default memo(MapChart);
