305bda4ff3aa641b7ea44549b44b8a2fd82fb505
SolbiShin <sbshin.dev@gmail.com>
2022-11-23 00:02:24 +0900

    불필요한 파일 제거 && 파일경로 수정

--- world-trip-app/src/App.tsx
+++ world-trip-app/src/App.tsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import './App.css';
-import MapChart from './MapChart';
+import MapChart from './components/MapChart';
 import ReactTooltip from "react-tooltip";
 
 function App() {
