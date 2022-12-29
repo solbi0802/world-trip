81a7c416bcf77006dc9a102fd7a0c0614184c15c
SolbiShin <sbshin.dev@gmail.com>
2022-12-30 00:19:19 +0900

    클릭이벤트 코드 정리

--- world-trip-app/src/components/MapChart.tsx
+++ world-trip-app/src/components/MapChart.tsx
@@ -1,36 +1,38 @@
 import React,{Dispatch, memo, SetStateAction, useState} from "react";
 import {ComposableMap, Geographies,Geography, ZoomableGroup } from 'react-simple-maps';
 
+const geoUrl = "/features.json";
+
 const MapChart = ({setTooltipContent}: {setTooltipContent: Dispatch<SetStateAction<string>>})  => {
-  const [countryList, setCountryList] = useState<Array<string>>([])
+  const [countryList, setCountryList] = useState([])
+
+  const handleClick = (geo) => {
+    console.log('클릭',geo.properties.name)
+    if(!countryList.includes(geo.properties.name)) 
+    setCountryList([...countryList, geo.properties.name])
+  };
+
   return (
     <div data-tip="">
     <h2>세계 여행 기록</h2>
     <div className="map-view">
     <ComposableMap>
-      <ZoomableGroup>
-         <Geographies geography="/features.json">
+         <Geographies geography={geoUrl}>
             {({ geographies }) =>
-              geographies.map((geo) => (
+              geographies.map((geo) => {
+                const isClicked = countryList === geo.properties.name;
+                console.log('isClicked',isClicked)
+                return (
                 <Geography
                   key={geo.rsmKey}
                   geography={geo}
+                  fill={isClicked ? "#E42": "#b7b7bb"}
                   onMouseEnter={() => {
                     setTooltipContent(geo.properties.kor ? `${geo.properties.kor}` : `${geo.properties.name}`);
                   }}
-                  onMouseLeave={() => {
-                    setTooltipContent("");
-                  }}
-                  onMouseDownCapture={() => {
-                    console.log('클릭',geo.properties.name)
-                    if(!countryList.includes(geo.properties.name)) 
-                    setCountryList([...countryList, geo.properties.name])
-                  }}
+                  onMouseLeave={() => setTooltipContent("")}
+                  onClick={() => handleClick(geo)}
                   style={{
-                    default: {
-                      fill: "#b7b7bb",
-                      outline: "none"
-                    },
                     hover: {
                       fill: "#97a3ea",
                       outline: "none"
@@ -40,22 +42,16 @@ const MapChart = ({setTooltipContent}: {setTooltipContent: Dispatch<SetStateActi
                       outline: "none"
                     },
                   }}
-                />
-              ))
-            }
+                  />
+                )
+            })
+          }
           </Geographies>
-      {/* <Marker coordinates={[0, 0]}>
-            <circle r={3} fill="#d586d3" />
-          </Marker> */}
-        </ZoomableGroup>
     </ComposableMap>
     </div>
     <div className="trip-list">
     <p>여행한 나라</p>
-    {countryList.join()}
     </div>
-    <img src="/airplain-out.gif"/>
-    <img src="/airplain-in.gif"/>
     </div>
   );
 }
